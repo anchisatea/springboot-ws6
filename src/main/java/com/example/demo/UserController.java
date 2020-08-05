b4baf6aa4ff7b2644b959d8acfd6098ec3ace6d6
Anchisa Teachalikhidkul <anchisatea@mathregit.group>
2020-08-05 13:46:14 +0700

    step02 - modified code

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -4,26 +4,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import  java.util.List;
 
 @RestController
 public class UserController {
 
-   /* @GetMapping("/users")
-    public List<UsersResponse> getAllUser(@RequestParam(required = false) int page, int itemPerPage){
+   @GetMapping("/users")
+    public PagingResponse getAllUser(@RequestParam(defaultValue = "1") int page,
+                                     @RequestParam(name = "item_per_page", defaultValue = "10") int itemPerPage){
+
+       PagingResponse pagingResponse = new PagingResponse(page, itemPerPage);
+
         List<UsersResponse> usersResponseList  = new ArrayList<>();
         usersResponseList.add(new UsersResponse(1, "User 1"));
         usersResponseList.add(new UsersResponse(2, "User 2"));
         usersResponseList.add(new UsersResponse(3, "User 3"));
-        return usersResponseList;
-    }
-*/
 
-    @GetMapping("/users")
-    public String getPage(@RequestParam(required = false, defaultValue = "1") String page, @RequestParam(name = "item_per_page",required =false, defaultValue = "10") String itemPerPage){
-        return "Page: " + page + ", itemPerPage: " + itemPerPage;
+        pagingResponse.setUsersResponse(usersResponseList);
+        return pagingResponse;
     }
+
+
+
+    //@GetMapping("/users")
+    //public String getPage(@RequestParam(required = false, defaultValue = "1") String page, @RequestParam(name = "item_per_page",required =false, defaultValue = "10") String itemPerPage){
+    //    return "Page: " + page + ", itemPerPage: " + itemPerPage;
+    //}
 
 
     @GetMapping("/users/{id}")
