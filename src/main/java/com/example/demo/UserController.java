c447af234f26516ee405ea7d9ad8ba554256170c
Anchisa Teachalikhidkul <anchisatea@mathregit.group>
2020-08-05 15:26:51 +0700

    Working with a repository

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,14 +1,30 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
 import java.util.ArrayList;
-import  java.util.List;
+import java.util.List;
 
 @RestController
 public class UserController {
 
-   @GetMapping("/users")
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/users")
+    public UsersResponse createNewUser(@RequestBody NewUserRequest request){
+        // Validate input
+        // Create user into database =>
+        User  user = new User();
+        user.setName((request.getName()));
+        user.setAge(request.getAge());
+        user = userRepository.save(user);
+        return new UsersResponse(user.getId(), user.getName() + user.getAge());
+    }
+
+    @GetMapping("/users")
     public PagingResponse getAllUser(@RequestParam(defaultValue = "1") int page,
                                      @RequestParam(name = "item_per_page", defaultValue = "10") int itemPerPage){
 
@@ -34,8 +50,9 @@ public class UserController {
         return new UsersResponse(id, "User " + id );
     }
 
-    @PostMapping("/users")
-    public UsersResponse createNewUser(@RequestBody NewUserRequest request){
-       return new UsersResponse(0, request.getName() + request.getAge());
+
+    @PostMapping("/users1")
+    public String createNewUserWithFormData(NewUserRequest request){
+       return request.getName() + request.getAge();
     }
 }
