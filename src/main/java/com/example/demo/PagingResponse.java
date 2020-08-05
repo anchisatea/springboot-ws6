6196c5c5b6ad691540fbd9b9790279589cd4fb38
Anchisa Teachalikhidkul <anchisatea@mathregit.group>
2020-08-05 14:21:29 +0700

    edit: JsonProperty

--- src/main/java/com/example/demo/PagingResponse.java
+++ src/main/java/com/example/demo/PagingResponse.java
@@ -1,14 +1,22 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.util.List;
 
-public class PagingResponse {
 
+@JsonPropertyOrder({"page", "ItemPerPage", "Users"})
+
+public class PagingResponse {
+    @JsonProperty("users")
     private List<UsersResponse> usersResponseList;
     private  int page;
+
+    @JsonProperty("item_per_page")
     private int itemPerPage;
 
-    public  PagingResponse(int page, int itemPerPage){
+    public  PagingResponse(int page, int itemPerPage) {
         this.page = page;
         this.itemPerPage = itemPerPage;
     }
@@ -18,30 +26,25 @@ public class PagingResponse {
 
     }
 
-
     public List<UsersResponse> getUsersResponseList() {
         return usersResponseList;
 
     }
-
 
     public int getPage() {
         return page;
 
     }
 
-
     public void setPage(int page) {
         this.page = page;
 
     }
 
-
     public int getItemPerPage() {
         return itemPerPage;
 
     }
-
 
     public void setItemPerPage(int itemPerPage) {
         this.itemPerPage = itemPerPage;
